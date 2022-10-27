a79bf5b4eeb7b25cf3645b560c7f1cf4982b067d
Carlos Eduardo Dal Molin <carlosdudu164@gmail.com>
2022-10-26 22:30:32 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/carlosmolin/hrworker/resources/WorkerResource.java
@@ -0,0 +1,31 @@
+package com.carlosmolin.hrworker.resources;
+
+import com.carlosmolin.hrworker.entities.Worker;
+import com.carlosmolin.hrworker.repositories.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResource {
+
+    @Autowired
+    private WorkerRepository workerRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Worker>> findAll() {
+        return ResponseEntity.ok(workerRepository.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(workerRepository.findById(id).get());
+    }
+}
