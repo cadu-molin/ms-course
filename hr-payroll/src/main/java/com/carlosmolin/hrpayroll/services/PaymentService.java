939b5370fdf1af1b10a8718f68ce65fd7b440e8c
Carlos Eduardo Dal Molin <carlos.molin@ciss.com.br>
2022-10-28 13:19:21 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/carlosmolin/hrpayroll/services/PaymentService.java
@@ -0,0 +1,12 @@
+package com.carlosmolin.hrpayroll.services;
+
+import com.carlosmolin.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(Long workerId, Integer days) {
+        return new Payment("Bob", 200.0, days);
+    }
+}
