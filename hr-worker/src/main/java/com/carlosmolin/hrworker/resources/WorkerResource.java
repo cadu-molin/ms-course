8b6f6c4dde892a9647223211ac8ac5b039fdceda
Carlos Eduardo Dal Molin <carlosdudu164@gmail.com>
2022-11-12 16:47:39 -0300

    Project hr-user created

--- hr-worker/src/main/java/com/carlosmolin/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/carlosmolin/hrworker/resources/WorkerResource.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RefreshScope
+@RefreshScope  
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
